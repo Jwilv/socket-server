7a1d8190389e2033e1aa132952a37228175954e6
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-01 17:56:14 -0300

    cliente emite al back, el back escucha

--- index.js
+++ index.js
@@ -10,8 +10,19 @@ app.use( express.static(__dirname + '/public'))
 const io = require('socket.io')(server);
 
 io.on('connection', ( socket ) => { 
-    socket.emit('ms-bienvenida','bienvenidos al himalaya')
+
+    socket.emit('ms-bienvenida',{
+        msg:'bienvenidos al himalaya',
+        fecha: new Date(),
+    });
+
+    socket.on('msg-cliente', ( data ) => { 
+        console.log(data)
+    });
+
 });
+
+
 
 server.listen(5050, ()=>{
     console.log(`server corriendo en el port:${5050}`);
