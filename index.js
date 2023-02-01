5dbc367c66db416794e911116f511f25da8eec4a
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-01 17:12:54 -0300

    despliegue del directorio publico

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const express = require('express')
 //Servidor de express
-const app = require('express')();
+const app = express();
 //Servidor de sockets
 const server = require('http').createServer(app);
-//
+//Directorio
+app.use( express.static(__dirname + '/public'))
+
 const io = require('socket.io')(server);
 
-io.on('connection', () => { /* … */ });
+io.on('connection', () => { 
+    console.log('cliente conectado');
+});
 
 server.listen(5050, ()=>{
     console.log(`server corriendo en el port:${5050}`);
